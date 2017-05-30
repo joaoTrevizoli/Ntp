f71a78e41ec0c6837e884582b161f2a78aabefd2
joao <joao@lab804.com.br>
2017-05-30 17:22:48 -0300

    Ntp class ready

---
+++ lib/Ntp/Ntp.cpp
@@ -0,0 +1,149 @@
+#include "Ntp.hpp"
+
+
+Ntp::Ntp(WiFiUDP& udp, int8_t offset, uint32_t updateInterval, const uint16_t port):
+  _port(port)
+{
+  this->_udp = &udp;
+  this->_offset = offset;
+  this->_updateInterval = updateInterval;
+}
+
+Ntp::Ntp(WiFiUDP& udp, const char* server, int8_t offset,\
+   int32_t updateInterval, const uint16_t port):
+   _server(server),
+   _port(port)
+{
+  this->_udp = &udp;
+  this->_offset = offset;
+  this->_updateInterval = updateInterval;
+}
+
+bool Ntp::begin()
+{
+  Serial.println(this->_server);
+  Serial.println(this->_updateInterval);
+  Serial.println(this->_port);
+  Serial.println(STRATUM_0);
+
+  this->_udp->begin(this->_port);
+  this->getNewHostIP();
+}
+
+void Ntp::getServerHost()
+{
+  Serial.println(this->_server);
+  Serial.print("Host ip: ");
+  Serial.println(this->_timeServerIP);
+}
+
+uint32_t Ntp::secondsSince1900(bool force)
+{
+  if(!force) this->update();
+  else this->updateForce();
+
+  return this->_secondsSince1900;
+}
+
+uint32_t Ntp::unixTimeStamp(bool force)
+{
+  if(!force) this->update();
+  else this->updateForce();
+
+  if(this->_secondsSince1900 != 0)
+  {
+    return this->_secondsSince1900 - SEVENTY_YEARS;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+
+void Ntp::formatPackage()
+{
+  memset(this->_packageBuffer, 0, NTP_PACKAGE_SIZE);
+  this->_packageBuffer[0] = (LI << 3 | NTP_VERSION) << 3 | NTP_PACKET_MODE;
+  this->_packageBuffer[1] = STRATUM_0;
+  this->_packageBuffer[2] = NTP_POOL;
+  this->_packageBuffer[3] = PEER_CLOCK_PRECISION;
+  this->_packageBuffer[12]  = 49;
+  this->_packageBuffer[13]  = 0x4E;
+  this->_packageBuffer[14]  = 49;
+  this->_packageBuffer[15]  = 52;
+}
+
+bool Ntp::getNewHostIP()
+{
+   #if NTP_DEBUG == 1
+      Serial.println("Getting new ip from server pool...");
+   #endif
+
+   if (WiFi.status() == WL_CONNECTED)
+   {
+     WiFi.hostByName(this->_server, this->_timeServerIP);
+     #if NTP_DEBUG == 1
+      Serial.print("host ip: ");
+      Serial.println(this->_timeServerIP);
+     #endif
+     return true;
+   }
+   else
+   {
+     #if NTP_DEBUG == 1
+      Serial.println("Failed to get new ip, WiFi disconnected");
+     #endif
+     return false;
+   }
+}
+
+bool Ntp::sendNTPpackage()
+{
+  this->formatPackage();
+
+  #if NTP_DEBUG == 1
+    Serial.println("sending NTP packet...");
+  #endif
+  // Serial.println(this->_timeServerIP);
+  this->_udp->beginPacket(this->_timeServerIP, NTP_HOST_PORT);
+  this->_udp->write(this->_packageBuffer, NTP_PACKAGE_SIZE);
+  this->_udp->endPacket();
+}
+
+bool Ntp::updateForce()
+{
+   this->sendNTPpackage();
+   byte timeOut = 0;
+   int8_t cb = 0;
+   do
+   {
+     delay(10);
+     cb = this->_udp->parsePacket();
+     if(timeOut > 100)
+     {
+      Serial.println("TimeOut, uneble to receive an package...");
+      return false;
+     }
+     timeOut++;
+   }while (cb==0);
+
+   this->_udp->read(this->_packageBuffer, NTP_PACKAGE_SIZE);
+   unsigned long highWord = word(this->_packageBuffer[40], this->_packageBuffer[41]);
+   unsigned long lowWord = word(this->_packageBuffer[42], this->_packageBuffer[43]);
+   this->_secondsSince1900 = highWord << 16 | lowWord;
+   return true;
+}
+
+bool Ntp::update()
+{
+  if ((millis() - this->_lastUpdate >= this->_updateInterval) \
+  || this->_lastUpdate == 0
+  || this->_secondsSince1900 == 0)
+  {
+    this->getNewHostIP();
+    this->_lastUpdate = millis();
+    return this->updateForce();
+  }
+  return true;
+}
--- lib/Ntp/Ntp.hpp
+++ lib/Ntp/Ntp.hpp
@@ -3,35 +3,66 @@
 
 #include "Arduino.h"
 #include "WiFiUdp.h"
+#include "ESP8266WiFi.h"
 
 #define NTP_DEBUG 1
+
+#define SEVENTY_YEARS 2208988800UL
+
+// NTP header
 #define SERVER_POOL_URL "br.pool.ntp.org"
 #define NTP_PACKAGE_SIZE 48
-
-enum {stratum_0, stratum_1, stratum_2, stratum_3};
+#define NTP_LOCAL_PORT 2390
+#define NTP_HOST_PORT 123
+#define LI 0b11
+#define NTP_VERSION 0b100
+#define NTP_PACKET_MODE 0b011
+#define STRATUM_0 0
+#define STRATUM_1 1
+#define STRATUM_2 2
+#define STRATUM_3 3
+#define NTP_POOL 6
+#define PEER_CLOCK_PRECISION 0XEC
 
 class Ntp
 {
 public:
-  Ntp(int8_t offset=0, int32_t updateInterval=10000, const uint16_t port=2390);
-  Ntp(const char* server, int8_t offset=0,\
-     int32_t updateInterval=10000, const uint16_t port=2390);
+  int8_t _offset;
+  WiFiUDP* _udp;
+
+  Ntp(WiFiUDP& udp, int8_t offset=0,\
+     uint32_t updateInterval=10000, \
+     const uint16_t port=NTP_LOCAL_PORT);
+
+  Ntp(WiFiUDP& udp, const char* server,\
+     int8_t offset=0,\
+     int32_t updateInterval=10000,\
+     const uint16_t port=NTP_LOCAL_PORT);
 
   bool begin();
   void getServerHost();
-  uint32_t secondsSince1900();
+
+  uint32_t secondsSince1900(bool force=false);
+  uint32_t unixTimeStamp(bool force=false);
 
 private:
-  const uint32_t seventyYears = 2208988800UL;
-  const uint8_t _port;
-  const char* _server;
+
+  uint32_t _updateInterval;
+  uint32_t _lastUpdate = 0;
+  uint32_t _secondsSince1900 = 0;
+  const uint16_t _port;
+  const char* _server = SERVER_POOL_URL;
+
 
   byte _packageBuffer[NTP_PACKAGE_SIZE];
   IPAddress _timeServerIP;
-  WiFiUDP _udp;
 
-
+  bool getNewHostIP();
+  void formatPackage();
   bool sendNTPpackage();
+  bool update();
+  bool updateForce();
+
 };
 
 #endif
